ac31493970ace164d5853b34af89af94d3b996ac
gibson <pwriter455@gmail.com>
2026-04-15 16:30:58 +0300

    Add page fade transition on route change (0.25s opacity fade)

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import NewsletterPopup from './NewsletterPopup'
 
 export default function Layout() {
+  const { pathname } = useLocation()
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Navbar />
       <main className="flex-1">
-        <Outlet />
+        <div key={pathname} className="animate-fade-in">
+          <Outlet />
+        </div>
       </main>
       <Footer />
       <NewsletterPopup />
